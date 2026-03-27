5b13f2113ff8b9718bef686401197adb6dd26571
Alberto <albertodominguezss.22@gmail.com>
2026-03-27 16:05:05 +0100

    Obtener histórico completo de precios por empresa

--- main.py
+++ main.py
@@ -11,20 +11,43 @@ def obtener_datos(simbolo, api_key):
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={simbolo}&apikey={api_key}"
     respuesta = requests.get(url)
     datos = respuesta.json()
+    historico = []
+
     if "Time Series (Daily)" not in datos:
         print(f"Error en {simbolo}: {datos.get('Information', 'Error desconocido')}")
         return None
     serie = datos["Time Series (Daily)"]
-    ultima_fecha = list(serie.keys())[0]
-    precio_cierre_ultima_fecha = serie[ultima_fecha]["4. close"]
-    return {"fecha": ultima_fecha, "cierre": precio_cierre_ultima_fecha}
+
+    for fecha, precios in serie.items():
+        historico.append({"fecha": fecha, "cierre": precios["4. close"]}) #en la BBDD cada fila sera un diccionario
+        
+    return historico
+
+
 
 empresas = ["AAPL", "GOOGL", "MSFT"]
 
 for empresa in empresas:
     resultado = obtener_datos(empresa, api_key)
+
     if resultado:
         print(f"{empresa} | {resultado['fecha']} | {resultado['cierre']}")
+    else:
+        print(f"{empresa} | sin datos disponibles")
+    time.sleep(15) #para que la API nos permita acceder
+
+    ultima_fecha = list(serie.keys())[0]
+    precio_cierre_ultima_fecha = serie[ultima_fecha]["4. close"]
+    return {"fecha": ultima_fecha, "cierre": precio_cierre_ultima_fecha}
+
+
+
+for empresa in empresas:
+    resultado = obtener_datos(empresa, api_key)
+
+    if resultado:
+        for dia in resultado: 
+            print(f"{empresa} | {dia['fecha']} | {dia['cierre']}")
     else:
         print(f"{empresa} | sin datos disponibles")
     time.sleep(15) #para que la API nos permita acceder
